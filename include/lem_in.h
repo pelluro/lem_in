a85ccacad689555c957d2b3756f462083d60c5ee
Minh PHAM <mipham@student.42.fr>
2017-12-28 20:10:22 +0100

    firt commit

---
+++ include/lem_in.h
@@ -0,0 +1,19 @@
+
+#ifndef LEM_IN_H
+#define LEM_IN_H
+
+# include "libft/libft.h"
+
+typedef struct s_map_read
+{
+	char *room;
+	char *room_list;
+	char *ants;
+	char *links;
+	int		nb_ants;
+	int		nb_room;
+	int		nb_links;
+} t_map_read;
+
+
+#endif
---
+++ src/main.c
@@ -0,0 +1,9 @@
+#include "../include/lem_in.h
+
+int main(void)
+{
+	t_map_read *map;
+	ft_read(m);
+
+	return(0);
+}
