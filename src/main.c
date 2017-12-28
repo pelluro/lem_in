19c52d592503fcb81941fafaf7b4d52e97b92fca
Minh PHAM <mipham@student.42.fr>
2017-12-28 21:30:47 +0100

    je commence doucement

--- src/main.c
+++ src/main.c
@@ -12,10 +12,26 @@ static int ft_isempty(char *s)
 	return (0);
 }
 
+static t_map_read	*ft_map(void)
+{
+	t_map_read *map;
+
+	map = (t_map_read*)ft_memalloc(sizeof(t_map_read));
+	if (map == NULL)
+		return (0);
+	map->ants = ft_strnew(1);
+	map->links = ft_strnew(1);
+	map->room = ft_strnew(1);
+	map->nb_ants = 0;
+	map->nb_room = 0;
+	map->nb_links = 0;
+	return (map);
+}
+
 int main(void)
 {
 	t_map_read *map;
-	ft_read(m);
+	ft_read_map(map);
 
 	return(0);
 }
