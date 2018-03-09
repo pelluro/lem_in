91bb79cae6f1f97f456e60f533949a52234fc674
Minh PHAM <mipham@student.42.fr>
2018-03-09 16:01:06 +0100

    add color so beautiful

---
+++ checkroom.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   checkroom.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mipham <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/01/03 15:53:41 by mipham            #+#    #+#             */
+/*   Updated: 2018/01/11 12:43:37 by mipham           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/lem_in.h"
+
+static void		is_number(char **r, t_map *m, char *s)
+{
+	int i;
+
+	i = 0;
+	if (s[0] == '-' || ft_isdigit(s[0]))
+	{
+		while (s[++i])
+		{
+			if (!ft_isdigit(s[i]))
+				free_tab(r, m, 1);
+		}
+	}
+	else
+		free_tab(r, m, 1);
+}
+
+static void		validate_room(t_map *m, char *line)
+{
+	char **r;
+
+	r = ft_strsplit(line, ' ');
+	if (r[0][0] == 'L' || r[3] != NULL)
+		free_tab(r, m, 1);
+	is_number(r, m, r[1]);
+	is_number(r, m, r[2]);
+	free_tab(r, m, 0);
+}
+
+void			rooms(t_map *m, char *line)
+{
+	m->started = 2;
+	m->rooms_list = join_str(m->rooms_list, line, 0);
+	if (line[0] == '#')
+		return ;
+	validate_room(m, line);
+	m->q_rooms++;
+}
+
+
---
+++ method.c
@@ -0,0 +1,77 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   method.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mipham <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/01/03 15:54:37 by mipham            #+#    #+#             */
+/*   Updated: 2018/01/11 14:02:43 by mipham           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/lem_in.h"
+
+char	*join_str(char *s1, char *s2, int clean)
+{
+	char *new_s;
+
+	new_s = ft_strnew(ft_strlen(s1) + ft_strlen(s2) + 1);
+	if (s1[0] == 0)
+	{
+		free(new_s);
+		free(s1);
+		return (s2);
+	}
+	ft_strcat(new_s, s1);
+	ft_strcat(new_s, "\n");
+	ft_strcat(new_s, s2);
+	ft_strdel(&s1);
+	(clean) ? ft_strdel(&s2) : 0;
+	return (new_s);
+}
+
+//links that was passed on arguments
+void	links(t_map *m, char *line)
+{
+	if (m->started == 2)
+		m->started = 3;
+	if (m->started != 3)
+		ft_exit(m, 1);
+	m->links = join_str(m->links, line, 0);
+}
+
+int		room_index(t_map *m, char *room_name, int start)
+{
+	int index;
+
+	index = (start) ? -1 : 0;
+	while (m->rooms[++index] && index < m->q_rooms)
+	{
+		if (ft_strcmp(m->rooms[index], room_name) == 0)
+			return (index);
+	}
+	return (index);
+}
+
+void	count_ants(t_map *m, char *line)
+{
+	int		i;
+	char	*s;
+
+	i = 0;
+	m->started = 1;
+	m->ants_str = join_str(m->ants_str, line, 0);
+	if (line[0] == '#')
+		return ;
+	s = ft_strtrim(line);
+	if ((m->ants = ft_atoi(s)) <= 0)
+		ft_exit(m, 1);
+	while (s[i] != '\n' && s[i] != 0)
+	{
+		if (!ft_isdigit(s[i]))
+			ft_exit(m, 1);
+		i++;
+	}
+	free(s);
+}
